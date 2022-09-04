09ed49acf50deb160d6d0208191e98eb31cddb20
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-09-04 14:38:43 +0100

    Refactor stg_metabase__report_dashboards

--- models/staging/stg_metabase__report_dashboards.sql
+++ models/staging/stg_metabase__report_dashboards.sql
@@ -1,1 +1,35 @@
-select * from {{ var('report_dashboard') }}
+with source as (
+
+  select * from {{ var('report_dashboard') }}
+
+),
+
+renamed as (
+
+  select
+
+    id as dashboard_id,
+    name as dashboard_name,
+    description as dashboard_description,
+    parameters,
+    points_of_interest,
+    caveats,
+    embedding_params,
+    position,
+    collection_position,
+    creator_id,
+    public_uuid,
+    made_public_by_id,
+    entity_id,
+    collection_id,
+    cache_ttl,
+    archived as is_archived,
+    show_in_getting_started as is_shown_in_getting_started,
+    enable_embedding as is_embedding_enabled,
+    created_at,
+    updated_at
+    
+  from source
+)
+
+select * from renamed
