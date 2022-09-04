9a84bb0d66853081f939d0da7e822227a4b00f72
jared.rimmer <jared.rimmer1+github@gmail.com>
2022-09-04 14:38:23 +0100

    Refactor stg_metabase__reports_cards

--- models/staging/stg_metabase__report_cards.sql
+++ models/staging/stg_metabase__report_cards.sql
@@ -1,1 +1,42 @@
-select * from {{ var('report_card') }}
+with source as (
+
+  select * from {{ var('report_card') }}
+
+),
+
+renamed as (
+
+  select
+
+    id as card_id,
+    name as card_name,
+    description as card_description,
+    display as display_type,
+    dataset_query,    
+    query_type,
+    visualization_settings,
+    cache_ttl,
+    result_metadata,
+    collection_position,
+    embedding_params,
+    parameters,
+    parameter_mappings,
+    creator_id,
+    database_id,
+    table_id,
+    collection_id,
+    public_uuid,
+    made_public_by_id,
+    entity_id,
+    archived as is_archived,
+    enable_embedding as is_embedding_enabled,
+    dataset as is_dataset,
+    collection_preview as is_collection_preview,
+    created_at,
+    updated_at
+  
+  from source
+
+)
+
+select * from renamed
